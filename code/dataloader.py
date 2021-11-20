a1c773a6694d724263a4d44b30bc10c0f25d2e8c
Daehyun Cho <1phantasmas@korea.ac.kr>
2021-11-20 16:14:00 +0900

    [preprocess] use spacy to tokenize and merge into dataloader pipeline

--- code/dataloader.py
+++ code/dataloader.py
@@ -1,9 +1,20 @@
 from pathlib import Path
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
-
 from datasets import load_from_disk
 import datasets
+
+from preprocess import Preprocessor
+
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - PID: %(process)d -  %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
 
 class PaperDataset(Dataset):
     def __init__(self, config, split="train"):
@@ -19,23 +30,17 @@ class PaperDataset(Dataset):
             Path(f"{config.data_dir}/{config.dataset_name}")
         )[split]
 
-        try:
-            # When cache file is used, below codes might raise error.
-            self.full_dataset = self.full_dataset.remove_columns("arxiv_id")
-            if not config.use_abstract:
-                self.full_dataset.remove_columns("abstract")
-
-            if not config.use_task_id:
-                self.full_dataset.remove_columns("task_id")
-        except:
-            pass
-
-        self.dataset = self.initialize_dataset(self.full_dataset, config.init_pct, split)
-
+        self.dataset = self.initialize_dataset(
+            self.full_dataset, config.init_pct, split
+        )
+        self.setup_preprocessor(config)
 
     def __getitem__(self, idx):
 
         # TODO preprocessing! - tokenize and map areas
+        data = self.dataset[idx]
+
+        title_token = self.preprocessor.tokenize(data["title"])
 
         return self.dataset[idx]
 
@@ -51,7 +56,7 @@ class PaperDataset(Dataset):
 
         """Get small portion from the whole for initial data.
 
-        This will 
+        This will
 
         If init_pct receives 1 or the dataset is not for training,
         this will automatically return the intact input.
@@ -59,19 +64,45 @@ class PaperDataset(Dataset):
         Returns:
             [datasets] contains with iniitial percentage of the whole data
         """
+        logger.info(f"Initialize {split.capitalize()} Dataset.")
+        try:
+            # When cache file is used, below codes might raise error.
+            if not self.config.use_abstract:
+                logger.info("Remove abstract.")
+                dataset = dataset.remove_columns("abstract")
+
+            if not self.config.use_task_id:
+                logger.info("Remove task_id")
+                dataset = dataset.remove_columns("task_id")
+
+            dataset = dataset.remove_columns("arxiv_id")
+
+        except:
+            logger.info("Using cached dataset, wasn't able to remove columns.")
+            pass
 
         if init_pct < 1 and split is "train":
+            logger.info(f"Use {init_pct}% of the total dataset.")
             pool_dataset, initial_dataset = train_test_split(
                 dataset, test_size=init_pct, shuffle=True, random_state=self.config.seed
             )
             self.pool_dataset = datasets.Dataset.from_dict(pool_dataset)
             initial_dataset = datasets.Dataset.from_dict(initial_dataset)
+            logger.info(f"Total {len(initial_dataset)} of papers will be used.")
 
             return initial_dataset
 
         else:
+            logger.info(
+                f"Use the full dataset, for {split} dataset of total {len(dataset)} papers."
+            )
             setattr(self, "pool_dataset", None)
             return dataset
+
+    def setup_preprocessor(self, config):
+
+        self.preprocessor = Preprocessor(config)
+        self.tokenize = self.preprocessor.tokenize
 
     def __repr__(self):
         return repr(self.dataset)
---
+++ code/preprocess.py
@@ -0,0 +1,52 @@
+import logging
+import torch
+import spacy
+
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - PID: %(process)d -  %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+
+class Preprocessor:
+    def __init__(self, config):
+
+        self.config = config
+        if not hasattr(config, "spacy_model"):
+            config.spacy_model = "en_core_web_trf"
+
+        self.setup_spacy()
+
+    def setup_spacy(self):
+
+        try:
+            self.tokenizer = spacy.load(self.config.spacy_model)
+            logger.info(
+                f"Successfully loaded Spacy Tokenizer, {self.config.spacy_model}"
+            )
+        except:
+            logger.warn(f"Failed to load Spacy Tokenizer, {self.config.spacy_model}")
+            raise
+
+    def tokenize(self, sentence: str) -> torch.Tensor:
+
+        if not hasattr(self, "tokenizer"):
+            self.setup_spacy()
+
+        tokenized = self.tokenizer(sentence)
+        return tokenized._.trf_data.tokens["input_ids"]
+
+
+if __name__ == "__main__":
+
+    from config import parse_arguments
+
+    data_args, training_args, model_args = parse_arguments()
+    preprocessor = Preprocessor(data_args)
+
+    document = "Spacy is fucking hard to use arghggh!"
+    print(preprocessor.tokenize(document))
