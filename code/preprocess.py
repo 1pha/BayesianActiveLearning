5b3485adfa67f4aa06f2495a2142d96ee3a64e41
Daehyun Cho <1phantasmas@korea.ac.kr>
2021-11-20 18:14:33 +0900

    [logger] edit format

--- code/preprocess.py
+++ code/preprocess.py
@@ -4,7 +4,7 @@ import torch
 import spacy
 
 logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    format="[%(asctime)s] %(levelname)s(%(name)s): %(message)s",
     datefmt="%m/%d/%Y %H:%M:%S",
     level=logging.INFO,
 )
