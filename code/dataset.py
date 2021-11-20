b5393df04b81b4e561f01715599b7d5394662041
Daehyun Cho <1phantasmas@korea.ac.kr>
2021-11-20 19:21:20 +0900

    [dataset] dataset is now preprocessed with datasets.map and batch_encode

---
+++ code/dataset.py
@@ -0,0 +1,123 @@
+import json
+import logging
+from pathlib import Path
+
+from sklearn.model_selection import train_test_split
+from datasets import load_from_disk
+import datasets
+from torch.utils.data.dataloader import DataLoader
+
+from preprocess import build_preprocessor
+
+logging.basicConfig(
+    format="[%(asctime)s] %(levelname)s - %(name)s: %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+
+def load_dataset(config, split):
+
+    return load_from_disk(Path(f"{config.data_dir}/{config.dataset_name}"))[split]
+
+
+def build_dataset(config, split):
+
+    full_dataset = load_dataset(config, split)
+    dataset = initialize_dataset(full_dataset, config, split)
+
+    preprocessor = build_preprocessor(config)
+    tokenize = preprocessor.batch_tokenize
+    area2idx = load_area2idx(config)
+
+    def batch_encode(example):
+
+        input_raw = (
+            example["title"]
+            if "abstract" not in example
+            else f"{example['title']}[SEP]{example['abstract']}"
+        )
+        input_ids = tokenize(input_raw)
+        label = [area2idx[area] for area in example["area"]]
+
+        return {
+            "input_ids": input_ids,
+            "label": label,
+        }
+
+    remove_columns = ["area", "title"]
+    remove_columns += ["abstract"] if "abstract" in dataset else []
+    dataset = dataset.map(batch_encode, batched=True, remove_columns=remove_columns)
+    return dataset
+
+
+def initialize_dataset(dataset, config, split):
+
+    """Get small portion from the whole for initial data.
+
+    This will
+
+    If init_pct receives 1 or the dataset is not for training,
+    this will automatically return the intact input.
+
+    Returns:
+        [datasets] contains with iniitial percentage of the whole data
+    """
+    logger.info(f"Initialize {split.capitalize()} Dataset.")
+    try:
+        # When cache file is used, below codes might raise error.
+        if not config.use_abstract:
+            logger.info("Remove abstract.")
+            dataset = dataset.remove_columns("abstract")
+
+        if not config.use_task_id:
+            logger.info("Remove task_id")
+            dataset = dataset.remove_columns("task_id")
+
+        dataset = dataset.remove_columns("arxiv_id")
+
+    except:
+        logger.info("Using cached dataset, wasn't able to remove columns.")
+        pass
+
+    if config.init_pct < 1 and split is "train":
+        logger.info(f"Use {config.init_pct}% of the total dataset.")
+        pool_dataset, initial_dataset = train_test_split(
+            dataset, test_size=config.init_pct, shuffle=True, random_state=config.seed
+        )
+        pool_dataset = datasets.Dataset.from_dict(pool_dataset)
+        initial_dataset = datasets.Dataset.from_dict(initial_dataset)
+        logger.info(f"Total {len(initial_dataset)} of papers will be used.")
+
+    else:
+        logger.info(
+            f"Use the full dataset, for {split} dataset of total {len(dataset)} papers."
+        )
+        initial_dataset = dataset
+
+    logger.info(f"{split.capitalize()} dataset was successfully initialized.")
+    return initial_dataset
+
+
+def load_area2idx(config):
+
+    fname = Path(f"{config.asset_dir}/{config.area2idx}")
+    try:
+        with open(fname) as f:
+            area2idx = json.load(f)
+        logger.info(f"Successfully loaded mapper file {fname}")
+        return area2idx
+
+    except:
+        logger.warn(f"Failed to load mapper file {fname} ")
+        raise
+
+
+if __name__ == "__main__":
+
+    from config import parse_arguments
+
+    data_args, training_args, model_args = parse_arguments()
+
+    print(build_dataset(data_args, "train"))
--- code/preprocess.py
+++ code/preprocess.py
@@ -4,7 +4,7 @@ import torch
 import spacy
 
 logging.basicConfig(
-    format="[%(asctime)s] %(levelname)s(%(name)s): %(message)s",
+    format="[%(asctime)s] %(levelname)s - %(name)s: %(message)s",
     datefmt="%m/%d/%Y %H:%M:%S",
     level=logging.INFO,
 )
@@ -18,6 +18,7 @@ class Preprocessor:
         if not hasattr(config, "spacy_model"):
             config.spacy_model = "en_core_web_trf"
 
+        self.max_seq_len = config.max_seq_len
         self.setup_spacy()
 
     def setup_spacy(self):
@@ -31,13 +32,39 @@ class Preprocessor:
             logger.warn(f"Failed to load Spacy Tokenizer, {self.config.spacy_model}")
             raise
 
-    def tokenize(self, sentence: str) -> torch.Tensor:
+    def tokenize(self, sentence) -> torch.Tensor:
 
         if not hasattr(self, "tokenizer"):
             self.setup_spacy()
 
-        tokenized = self.tokenizer(sentence)
-        return tokenized._.trf_data.tokens["input_ids"]
+        if isinstance(sentence, str):
+
+            # single instance will return tensor
+            tokenized = self.tokenizer(sentence)
+            return tokenized._.trf_data.tokens["input_ids"]
+
+    def batch_tokenize(self, sentences: list) -> list:
+
+        return [self.tokenize(s).squeeze().tolist() for s in sentences]
+
+    def truncate(self, input_ids):
+
+        seq_len = len(input_ids)
+        if seq_len > self.config.max_seq_len:
+            input_ids = input_ids[: self.config.max_seq_len]
+            seq_len = self.config.max_seq_len
+
+        else:
+            zero_seq = torch.zeros(self.config.max_seq_len, dtype=torch.long)
+            zero_seq[:seq_len] = input_ids
+            input_ids, zero_seq = zero_seq, input_ids
+
+        return input_ids
+
+
+def build_preprocessor(config):
+
+    return Preprocessor(config)
 
 
 if __name__ == "__main__":
