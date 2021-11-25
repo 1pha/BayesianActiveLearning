9a81a9667b15d534534c339b75f71c97126bb095
1pha <1phantasmas@korea.ac.kr>
2021-11-25 21:23:59 +0900

    [models] add bilstm, cnnlstm

--- code/load_model.py
+++ code/load_model.py
@@ -1,6 +1,6 @@
-from models import Bert
+from models import Bert, CNN_MC, BiLSTM_MC
 
-MODEL_DICT = {"bert": Bert}
+MODEL_DICT = {"bert": Bert, "bilstm": BiLSTM_MC, "cnnlstm": CNN_MC}
 
 
 def load_model(model_args):
@@ -8,3 +8,12 @@ def load_model(model_args):
     model_name_or_path = model_args.model_name_or_path
     model = MODEL_DICT[model_name_or_path.lower()](model_args)
     return model
+
+
+if __name__ == "__main__":
+
+    from config import parse_arguments
+
+    data_args, training_args, model_args = parse_arguments()
+
+    print(load_model(model_args))
--- code/models/__init__.py
+++ code/models/__init__.py
@@ -1,4 +1,6 @@
 from .transformer_models import Bert
+from .recurrent_cnn_models import CNN_MC
+from .recurrent_models import BiLSTM_MC
 
-__all__ = ["Bert"]
+__all__ = ["Bert", "CNN_MC", "BiLSTM_MC"]
 model_list = __all__
