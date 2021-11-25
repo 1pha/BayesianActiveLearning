fa2877d1eef9f3d1e17a01ddf9562312559cf3de
1pha <1phantasmas@korea.ac.kr>
2021-11-25 21:55:26 +0900

    [sweep] run multiple runs with variation of init_pct

--- sweep.bat
+++ sweep.bat
@@ -1,17 +1,20 @@
-python code/run.py --do_train=True --do_valid=True --do_test=True --output_dir=output/active_learning/bert_lc_init10 --checkpoint_period=1 --active_learning=True --acquisition=lc --acquisition_period=5 --init_pct=0.1
-python code/run.py --do_train=True --do_valid=True --do_test=True --output_dir=output/active_learning/bert_margin_init10 --checkpoint_period=1 --active_learning=True --acquisition=margin --acquisition_period=5 --init_pct=0.1
-python code/run.py --do_train=True --do_valid=True --do_test=True --output_dir=output/active_learning/bert_random_init10 --checkpoint_period=1 --active_learning=True --acquisition=random --acquisition_period=5 --init_pct=0.1
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.01 --model_name_or_path=bilstm --num_train_epochs=150 --intermediate_size=256 --output_dir=output/init_pct/001_bilstm
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.05 --model_name_or_path=bilstm --num_train_epochs=150 --intermediate_size=256 --output_dir=output/init_pct/005_bilstm
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.10 --model_name_or_path=bilstm --num_train_epochs=150 --intermediate_size=256 --output_dir=output/init_pct/010_bilstm
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.20 --model_name_or_path=bilstm --num_train_epochs=150 --intermediate_size=256 --output_dir=output/init_pct/020_bilstm
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.50 --model_name_or_path=bilstm --num_train_epochs=150 --intermediate_size=256 --output_dir=output/init_pct/050_bilstm
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=1.00 --model_name_or_path=bilstm --num_train_epochs=150 --intermediate_size=256 --output_dir=output/init_pct/100_bilstm
 
-python code/run.py --do_train=True --do_valid=True --do_test=True --output_dir=output/active_learning/bert_lc_init20 --checkpoint_period=1 --active_learning=True --acquisition=lc --acquisition_period=5 --init_pct=0.2
-python code/run.py --do_train=True --do_valid=True --do_test=True --output_dir=output/active_learning/bert_margin_init20 --checkpoint_period=1 --active_learning=True --acquisition=margin --acquisition_period=5 --init_pct=0.2
-python code/run.py --do_train=True --do_valid=True --do_test=True --output_dir=output/active_learning/bert_random_init20 --checkpoint_period=1 --active_learning=True --acquisition=random --acquisition_period=5 --init_pct=0.2
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.01 --model_name_or_path=cnnlstm --num_train_epochs=150  --output_dir=output/init_pct/001_cnnlstm
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.05 --model_name_or_path=cnnlstm --num_train_epochs=150  --output_dir=output/init_pct/005_cnnlstm
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.10 --model_name_or_path=cnnlstm --num_train_epochs=150  --output_dir=output/init_pct/010_cnnlstm
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.20 --model_name_or_path=cnnlstm --num_train_epochs=150 --output_dir=output/init_pct/020_cnnlstm
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.50 --model_name_or_path=cnnlstm --num_train_epochs=150 --output_dir=output/init_pct/050_cnnlstm
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=1.00 --model_name_or_path=cnnlstm --num_train_epochs=150 --output_dir=output/init_pct/100_cnnlstm
 
-python code/run.py --do_train=True --do_valid=True --do_test=True --output_dir=output/active_learning/bert_lc_init30 --checkpoint_period=1 --active_learning=True --acquisition=lc --acquisition_period=5 --init_pct=0.3
-python code/run.py --do_train=True --do_valid=True --do_test=True --output_dir=output/active_learning/bert_margin_init30 --checkpoint_period=1 --active_learning=True --acquisition=margin --acquisition_period=5 --init_pct=0.3
-python code/run.py --do_train=True --do_valid=True --do_test=True --output_dir=output/active_learning/bert_random_init30 --checkpoint_period=1 --active_learning=True --acquisition=random --acquisition_period=5 --init_pct=0.3
-
-python code/run.py --do_train=True --do_valid=True --do_test=True --output_dir=output/active_learning/bert_lc_init50 --checkpoint_period=1 --active_learning=True --acquisition=lc --acquisition_period=5 --init_pct=0.5
-python code/run.py --do_train=True --do_valid=True --do_test=True --output_dir=output/active_learning/bert_margin_init50 --checkpoint_period=1 --active_learning=True --acquisition=margin --acquisition_period=5 --init_pct=0.5
-python code/run.py --do_train=True --do_valid=True --do_test=True --output_dir=output/active_learning/bert_random_init50 --checkpoint_period=1 --active_learning=True --acquisition=random --acquisition_period=5 --init_pct=0.5
- 
-
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.01 --model_name_or_path=bert --num_train_epochs=150  --output_dir=output/init_pct/001_bert
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.05 --model_name_or_path=bert --num_train_epochs=150  --output_dir=output/init_pct/005_bert
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.10 --model_name_or_path=bert --num_train_epochs=150  --output_dir=output/init_pct/010_bert
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.20 --model_name_or_path=bert --num_train_epochs=150 --output_dir=output/init_pct/020_bert
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=0.50 --model_name_or_path=bert --num_train_epochs=150 --output_dir=output/init_pct/050_bert
+python code/run.py --do_train=True --do_valid=True --do_test=True --init_pct=1.00 --model_name_or_path=bert --num_train_epochs=150 --output_dir=output/init_pct/100_bert
