6d4eeb748dd49bb249476c28eba045fb9be31837
1pha <1phantasmas@korea.ac.kr>
2021-11-27 14:30:01 +0900

    [sweep] for active learning

--- sweep_active.bat
+++ sweep_active.bat
@@ -1,11 +1,25 @@
-python code/run.py --do_train=True --do_valid=True --model_name_or_path=bilstm --output_dir=output/active/bilstm_random --active_learning=True --acquisition=random --acquisition_period=2 --load_size=base
-python code/run.py --do_train=True --do_valid=True --model_name_or_path=bilstm --output_dir=output/active/bilstm_lc --active_learning=True --acquisition=lc --acquisition_period=2 --load_size=base
-python code/run.py --do_train=True --do_valid=True --model_name_or_path=bilstm --output_dir=output/active/bilstm_margin --active_learning=True --acquisition=margin --acquisition_period=2 --load_size=base
+echo code/run.py --do_train=True --do_valid=True --model_name_or_path=bilstm --output_dir=output/active/bilstm_random --active_learning=True --acquisition=random --acquisition_period=5 --load_size=base --increment_num=1500
+echo code/run.py --do_train=True --do_valid=True --model_name_or_path=bilstm --output_dir=output/active/bilstm_lc --active_learning=True --acquisition=lc --acquisition_period=5 --load_size=base --increment_num=1500
+python code/run.py --do_train=True --do_valid=True --model_name_or_path=bilstm --output_dir=output/active/bilstm_lc --active_learning=True --acquisition=lc --acquisition_period=5 --load_size=base --increment_num=1500
+echo code/run.py --do_train=True --do_valid=True --model_name_or_path=bilstm --output_dir=output/active/bilstm_random --active_learning=True --acquisition=random --acquisition_period=5 --load_size=base --increment_num=1500
+python code/run.py --do_train=True --do_valid=True --model_name_or_path=bilstm --output_dir=output/active/bilstm_margin --active_learning=True --acquisition=margin --acquisition_period=5 --load_size=base --increment_num=1500
 
-python code/run.py --do_train=True --do_valid=True --model_name_or_path=cnnlstm --output_dir=output/active/cnnlstm_random --active_learning=True --acquisition=random --acquisition_period=2 --load_size=base
-python code/run.py --do_train=True --do_valid=True --model_name_or_path=cnnlstm --output_dir=output/active/cnnlstm_lc --active_learning=True --acquisition=lc --acquisition_period=2 --load_size=base
-python code/run.py --do_train=True --do_valid=True --model_name_or_path=cnnlstm --output_dir=output/active/cnnlstm_margin --active_learning=True --acquisition=margin --acquisition_period=2 --load_size=base
+echo code/run.py --do_train=True --do_valid=True --model_name_or_path=cnnlstm --output_dir=output/active/cnnlstm_random --active_learning=True --acquisition=random --acquisition_period=5 --load_size=base --increment_num=1500
+python code/run.py --do_train=True --do_valid=True --model_name_or_path=cnnlstm --output_dir=output/active/cnnlstm_lc --active_learning=True --acquisition=lc --acquisition_period=5 --load_size=base --increment_num=1500
+python code/run.py --do_train=True --do_valid=True --model_name_or_path=cnnlstm --output_dir=output/active/cnnlstm_margin --active_learning=True --acquisition=margin --acquisition_period=5 --load_size=base --increment_num=1500
 
-python code/run.py --do_train=True --do_valid=True --model_name_or_path=bert --output_dir=output/active/bert_random --active_learning=True --acquisition=random --acquisition_period=2 --load_size=base
-python code/run.py --do_train=True --do_valid=True --model_name_or_path=bert --output_dir=output/active/bert_lc --active_learning=True --acquisition=lc --acquisition_period=2 --load_size=base
-python code/run.py --do_train=True --do_valid=True --model_name_or_path=bert --output_dir=output/active/bert_margin --active_learning=True --acquisition=margin --acquisition_period=2 --load_size=base
+echo code/run.py --do_train=True --do_valid=True --model_name_or_path=bert --output_dir=output/active/bert_random --active_learning=True --acquisition=random --acquisition_period=5 --load_size=base --increment_num=1500
+python code/run.py --do_train=True --do_valid=True --model_name_or_path=bert --output_dir=output/active/bert_lc --active_learning=True --acquisition=lc --acquisition_period=5 --load_size=base --increment_num=1500
+python code/run.py --do_train=True --do_valid=True --model_name_or_path=bert --output_dir=output/active/bert_margin --active_learning=True --acquisition=margin --acquisition_period=5 --load_size=base --increment_num=1500
+
+echo code/run.py --do_train=True --do_valid=True --model_name_or_path=bilstm --output_dir=output/active/bilstm_lc --active_learning=True --acquisition=lc --acquisition_period=10 --load_size=base --increment_num=1500
+python code/run.py --do_train=True --do_valid=True --model_name_or_path=bilstm --output_dir=output/active/bilstm_random --active_learning=True --acquisition=random --acquisition_period=10 --load_size=base --increment_num=1500
+python code/run.py --do_train=True --do_valid=True --model_name_or_path=bilstm --output_dir=output/active/bilstm_margin --active_learning=True --acquisition=margin --acquisition_period=10 --load_size=base --increment_num=1500
+
+echo code/run.py --do_train=True --do_valid=True --model_name_or_path=cnnlstm --output_dir=output/active/cnnlstm_random --active_learning=True --acquisition=random --acquisition_period=10 --load_size=base --increment_num=1500
+python code/run.py --do_train=True --do_valid=True --model_name_or_path=cnnlstm --output_dir=output/active/cnnlstm_lc --active_learning=True --acquisition=lc --acquisition_period=10 --load_size=base --increment_num=1500
+python code/run.py --do_train=True --do_valid=True --model_name_or_path=cnnlstm --output_dir=output/active/cnnlstm_margin --active_learning=True --acquisition=margin --acquisition_period=10 --load_size=base --increment_num=1500
+
+echo code/run.py --do_train=True --do_valid=True --model_name_or_path=bert --output_dir=output/active/bert_random --active_learning=True --acquisition=random --acquisition_period=10 --load_size=base --increment_num=1500
+python code/run.py --do_train=True --do_valid=True --model_name_or_path=bert --output_dir=output/active/bert_lc --active_learning=True --acquisition=lc --acquisition_period=10 --load_size=base --increment_num=1500
+python code/run.py --do_train=True --do_valid=True --model_name_or_path=bert --output_dir=output/active/bert_margin --active_learning=True --acquisition=margin --acquisition_period=10 --load_size=base --increment_num=1500
